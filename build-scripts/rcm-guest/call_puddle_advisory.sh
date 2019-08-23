6b5b624a137b763c01a09f83d39bbe19bdd19985
Tim Bielawa <tbielawa@redhat.com>
2019-08-23 09:47:23 -0500

    Test hypothesis that signed puddles require legit kerberos ticket

--- build-scripts/rcm-guest/call_puddle_advisory.sh
+++ build-scripts/rcm-guest/call_puddle_advisory.sh
@@ -9,7 +9,8 @@ CONF=`mktemp`
 echo "Making new signed compose for OCP ${VERSION} using Errata Whitelist: '${ERRATA}'"
 
 echo "Fetching default errata-puddle config file for ${VERSION}"
-wget https://raw.githubusercontent.com/openshift/aos-cd-jobs/master/build-scripts/puddle-conf/errata-puddle-${VERSION}-signed.conf -O $CONF
+# wget https://raw.githubusercontent.com/openshift/aos-cd-jobs/master/build-scripts/puddle-conf/errata-puddle-${VERSION}-signed.conf -O $CONF
+wget https://raw.githubusercontent.com/openshift/aos-cd-jobs/master/build-scripts/puddle-conf/atomic_openshift-${VERSION}.conf -O $CONF
 
 echo "Substituting in errata_whitelist with provided value: ${ERRATA}"
 sed -i -r "s|errata_whitelist.*|errata_whitelist = ${ERRATA}|" $CONF

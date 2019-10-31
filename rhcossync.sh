b967fb5040039d818df65aebf6411db08540858b
Tim Bielawa <tbielawa@redhat.com>
2019-10-31 11:03:38 -0500

    Make that directory

--- rhcossync.sh
+++ rhcossync.sh
@@ -7,17 +7,24 @@ VERSION=${1}
 RHCOS_MIRROR_PREFIX=${2}
 # Plain text file with items to download
 SYNCLIST=${3}
-
+FORCE=0
 # Put the items into this directory, we'll have to make it
 DESTDIR="/srv/pub/openshift-v4/dependencies/rhcos/${RHCOS_MIRROR_PREFIX}/${VERSION}"
 
 function checkDestDir() {
-    # DIR safety
-    #
-    # Check if target dir exists
-    #     explode if it doesn't and FORCE is not applied
-    #  Make dir if it doesn't exist
-    :
+    if [ -d "${DESTDIR}" ]; then
+	# Is this forced?
+	if [ "${FORCE}" == "0" ]; then
+	    echo "ERROR: Destination directory already exists and --force was not given"
+	    echo "ERROR: Run this script again with the --force option to continue"
+	    exit 1
+	else
+	    echo "INFO: Destination dir exists, will overwrite contents because --force was given"
+	fi
+    else
+	echo "INFO: Destination dir does not exist, will create it"
+	mkdir -p $DESTDIR
+    fi
 }
 
 

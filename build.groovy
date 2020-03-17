8a036d7dc9dd1d1d53ef7f18e505e069e6a6bb1a
Tim Bielawa <tbielawa@redhat.com>
2020-03-17 11:47:06 -0500

    Test updates from https://github.com/openshift/aos-cd-jobs/pull/2157/files

--- build.groovy
+++ build.groovy
@@ -10,10 +10,6 @@ artifacts = []
 // ######################################################################
 // Single arch options
 
-// Location of the SRC=DEST input file
-ocMirrorInput = "${mirrorWorking}/oc_mirror_input"
-// Location of the image stream to apply
-ocIsObject = "${mirrorWorking}/release-is.yaml"
 // Locally stored image stream stub
 baseImageStream = "/home/jenkins/base-art-latest-imagestream-${params.BUILD_VERSION}.yaml"
 // Kubeconfig allowing ART to interact with api.ci.openshift.org
@@ -33,7 +29,17 @@ multiarch = false
 
 def initialize() {
     buildlib.cleanWorkdir(mirrorWorking)
-    arches = buildlib.branch_arches("openshift-${params.BUILD_VERSION}")
+    arches = buildlib.branch_arches("openshift-${params.BUILD_VERSION}").toList()
+
+    /**
+     * We need to process x86_64 for some of our work. This is because building
+     * non-x86 releases requires the x86_64 imagestream to have been populated
+     * with a 'cli' tag, which the release controller will then use for non-x86
+     * release payloads.
+     */
+    arches.remove('x86_64')
+    arches.add(0, 'x86_64')
+
     multiarch = arches.size() > 1
     if ( params.NOOP) {
 	dryRun = "--dry-run=true"
@@ -44,7 +50,7 @@ def initialize() {
 	currentBuild.description = "Arches: ${arches.join(', ')}"
     } else {
 	currentBuild.displayName += " OCP: ${params.BUILD_VERSION}"
-	currentBuild.description = "Arches: default (amd64)"
+	currentBuild.description = "Arches: default (x86_64)"
 	artifacts.addAll(["MIRROR_working/oc_mirror_input", "MIRROR_working/release-is.yaml"])
     }
 
@@ -66,23 +72,18 @@ def initialize() {
 // file(s). Will take a few minutes because we must query brew and the
 // registry for each image.
 def buildSyncGenInputs() {
-    if ( multiarch ) {
 	echo("Generating SRC=DEST and ImageStreams for arches")
 	buildlib.doozer """
 ${images}
 --working-dir "${mirrorWorking}" --group 'openshift-${params.BUILD_VERSION}'
-release:gen-multiarch-payload
+release:gen-payload
 --is-name ${params.BUILD_VERSION}-art-latest
 --organization ${params.ORGANIZATION}
 --repository ${params.REPOSITORY}
 """
 	echo("Generated files:")
 	echo("######################################################################")
-	sh("cat src_dest")
 	for ( String a: arches ) {
-	    if ( a == "x86_64" ) {
-		continue
-	    }
 	    echo("######################################################################")
 	    echo("src_dest.${a}")
 	    sh("cat src_dest.${a}")
@@ -91,22 +92,6 @@ release:gen-multiarch-payload
 	sh("mv src_dest* ${mirrorWorking}/")
 	sh("mv image_stream*.yaml ${mirrorWorking}/")
 	artifacts.addAll(["MIRROR_working/src_dest*", "MIRROR_working/image_stream*"])
-    } else {
-	echo("Generating SRC=DEST for one arch")
-	buildlib.doozer """
-${images}
---working-dir "${mirrorWorking}" --group 'openshift-${params.BUILD_VERSION}'
-release:gen-payload
---src-dest ${ocMirrorInput}
---image-stream ${ocIsObject}
---is-base ${baseImageStream}
-'${ocFmtStr}'
-"""
-	echo("Generated file:")
-	echo("######################################################################")
-	echo("${ocMirrorInput}")
-	sh("cat ${ocMirrorInput}")
-    }
 }
 
 // ######################################################################
@@ -114,64 +99,58 @@ release:gen-payload
 // loop because it is known to fail occassionally depending on the
 // health of the source/destination endpoints.
 def buildSyncMirrorImages() {
-    if ( multiarch ) {
-	retry ( 3 ) {
-	    echo("Attempting to mirror default arch: amd64")
-	    // Always login again. It may expire between loops
-	    // depending on amount of time elapsed
-	    buildlib.registry_quay_dev_login()
-	    buildlib.oc "${logLevel} image mirror ${dryRun} --filename=${mirrorWorking}/src_dest"
-	    currentBuild.description += "\nMirrored: amd64"
-	}
-	// And again for each other arch
-	for ( String arch: arches ) {
-	    if ( arch == "x86_64" ) {
-		continue
-	    }
-
-	    retry ( 3 ) {
-		echo("Attempting to mirror arch: ${arch}")
-		// Always login again. It may expire between loops
-		// depending on amount of time elapsed
-		buildlib.registry_quay_dev_login()
-		buildlib.oc "${logLevel} image mirror ${dryRun} --filename=${mirrorWorking}/src_dest.${arch} --filter-by-os=${arch}"
-		currentBuild.description += ", ${arch}"
-	    }
-	}
-    } else {
-	retry ( 3 ) {
-	    buildlib.registry_quay_dev_login()
-	    buildlib.oc "${logLevel} image mirror ${dryRun} --filename=${ocMirrorInput}"
-	    currentBuild.description += "\nMirrored: default arch (amd64)"
-	}
+    for ( String arch: arches ) {
+        retry ( 3 ) {
+            echo("Attempting to mirror arch: ${arch}")
+            // Always login again. It may expire between loops
+            // depending on amount of time elapsed
+            buildlib.registry_quay_dev_login()
+            buildlib.oc "${logLevel} image mirror ${dryRun} --filename=${mirrorWorking}/src_dest.${arch}"
+        }
     }
 }
 
 
 def buildSyncApplyImageStreams() {
-    if ( multiarch ) {
-	echo("Updating ImageStream's")
+    echo("Updating ImageStream's")
+    for ( String arch: arches ) {
+        // Why be consistent when we could have more edge cases instead?
+        def a = (arch == "x86_64")? "": "-${arch}"
+        def theStream = "${params.BUILD_VERSION}-art-latest${a}"
 
-	echo("Going to apply this ImageStream:")
-	sh("cat ${mirrorWorking}/image_stream.yaml")
+        // Get the current IS and save it to disk. We may need this for debugging.
+        def currentIS = getImageStream(theStream, arch)
+        writeJSON(file: "pre-apply-${theStream}.json", input: currentIS)
+        artifacts.addAll(["pre-apply-${theStream}.json"])
 
-	buildlib.oc "${logLevel} apply ${dryRun} --filename=${mirrorWorking}/image_stream.yaml --kubeconfig ${ciKubeconfig}"
-	currentBuild.description += "\nUpdated ImageStream: amd64"
+        // We check for updates by comparing the object's 'generation'
+        def currentGeneration = currentIS.metadata.generation
+        echo("Current generation for ${theStream}: ${currentGeneration}")
 
-	// And one more time for each other stream
-	for ( String arch: arches ) {
-	    if ( arch == "x86_64" ) {
-		continue
-	    }
-	    echo("Going to apply this ImageStream:")
-	    sh("cat ${mirrorWorking}/image_stream.${arch}.yaml")
-	    buildlib.oc "${logLevel} apply ${dryRun} --filename=${mirrorWorking}/image_stream.${arch}.yaml --kubeconfig ${ciKubeconfig}"
-	    currentBuild.description += ", ${arch}"
-	}
-    } else {
-	buildlib.oc "${logLevel} apply ${dryRun} --filename=${ocIsObject} --kubeconfig ${ciKubeconfig}"
-	currentBuild.description += "\nUpdated default ImageStream (amd64)"
+        // Ok, try the update. Jack that debug output up high, just in case
+        echo("Going to apply this ImageStream:")
+        sh("cat ${mirrorWorking}/image_stream.${arch}.yaml")
+        buildlib.oc(" --loglevel=8 apply ${dryRun} --filename=${mirrorWorking}/image_stream.${arch}.yaml --kubeconfig ${ciKubeconfig}")
+
+        // Now we verify that the change went through and save the bits as we go
+        def newIS = getImageStream(theStream, arch)
+        writeJSON(file: "post-apply-${theStream}.json", input: newIS)
+        artifacts.addAll(["post-apply-${theStream}.json"])
+        def newGeneration = newIS.metadata.generation
+        if ( newGeneration == currentGeneration ) {
+            echo("IS `.metadata.generation` has not updated, it should have updated. Please use the debug info above to report this issue")
+            throw new Exception("Image Stream ${theStream} did not update")
+        }
     }
+}
+
+// Get a JSON object of the named image stream in the ocp
+// namespace. The image stream is also saved locally for debugging
+// purposes.
+def getImageStream(is, arch) {
+    def a = (arch == "x86_64")? "ocp": "ocp-${arch}"
+    def isJson = readJSON(text: buildlib.oc(" get is ${is} -n ${a} -o json --kubeconfig ${ciKubeconfig}").stdout)
+    return isJson
 }
 
 return this
